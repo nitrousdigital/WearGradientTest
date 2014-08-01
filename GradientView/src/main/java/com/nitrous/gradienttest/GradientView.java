75d58220099196396b9f36b929f3e02f45d969d5
Nick Kerr <nitrousdigital@yahoo.com>
2014-08-01 13:38:19 -0400

    Try painting to an offscreen bitmap... Try changing the Window Pixel Format...

--- GradientView/src/main/java/com/nitrous/gradienttest/GradientView.java
+++ GradientView/src/main/java/com/nitrous/gradienttest/GradientView.java
@@ -1,6 +1,7 @@
 package com.nitrous.gradienttest;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RadialGradient;
@@ -15,7 +16,6 @@ import android.view.View;
  * Created by nick on 8/1/14.
  */
 public class GradientView extends View {
-    private RectF rect;
     private Paint gradientFill;
     private int gradientStart;
     private int gradientEnd;
@@ -43,18 +43,28 @@ public class GradientView extends View {
         gradientFill.setDither(true);
         gradientFill.setAntiAlias(true);
         gradientFill.setStyle(Paint.Style.FILL);
-        rect = new RectF(0.0F, 0.0F, 1f, 1f);
     }
 
     public void draw(Canvas canvas) {
         super.draw(canvas);
-        canvas.drawRect(rect, gradientFill);
+
+        if (bmp != null) {
+            canvas.drawBitmap(bmp, 0,0, null);
+        }
     }
+
+    private Bitmap bmp;
 
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        rect = new RectF(0.0F, 0.0F, w, h);
 
+        if (bmp != null) {
+            bmp.recycle();
+            bmp = null;
+            Runtime.getRuntime().gc();
+        }
+
+        RectF rect = new RectF(0.0F, 0.0F, w, h);
         gradientFill.setShader(new RadialGradient(
                 rect.width() / 2.0F,
                 rect.height() / 2.0f,
@@ -62,6 +72,12 @@ public class GradientView extends View {
                 gradientStart,
                 gradientEnd,
                 Shader.TileMode.CLAMP));
+        gradientFill.setDither(true);
+        bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bmp);
+        canvas.drawRect(rect, gradientFill);
+
+
         postInvalidate();
     }
 
--- wear/src/main/java/com/nitrousdigital/gradienttest/WearGradientActivity.java
+++ wear/src/main/java/com/nitrousdigital/gradienttest/WearGradientActivity.java
@@ -1,8 +1,11 @@
 package com.nitrousdigital.gradienttest;
 
 import android.app.Activity;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class WearGradientActivity extends Activity {
@@ -12,6 +15,7 @@ public class WearGradientActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_wear_gradient);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
@@ -20,5 +24,14 @@ public class WearGradientActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DITHER);
+
     }
 }
