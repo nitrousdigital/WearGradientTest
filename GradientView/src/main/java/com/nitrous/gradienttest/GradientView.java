86103bcdefb688547616e8fef6d644f3ed9eaf25
Nick Kerr <nitrousdigital@yahoo.com>
2014-08-01 14:57:04 -0400

    removed 32 bit bitmap approach since it doesnt help.

--- GradientView/src/main/java/com/nitrous/gradienttest/GradientView.java
+++ GradientView/src/main/java/com/nitrous/gradienttest/GradientView.java
@@ -16,9 +16,10 @@ import android.view.View;
  * Created by nick on 8/1/14.
  */
 public class GradientView extends View {
+    private RectF rect;
     private Paint gradientFill;
-    private int gradientStart;
-    private int gradientEnd;
+    private int gradientStartColor;
+    private int gradientEndColor;
 
     public GradientView(Context context) {
         super(context);
@@ -37,46 +38,35 @@ public class GradientView extends View {
 
 
     private void init(Context ctx) {
-        gradientStart = ctx.getResources().getColor(R.color.gradient_start);
-        gradientEnd = ctx.getResources().getColor(R.color.gradient_end);
+        gradientStartColor = ctx.getResources().getColor(R.color.gradient_start);
+        gradientEndColor = ctx.getResources().getColor(R.color.gradient_end);
         gradientFill = new Paint();
         gradientFill.setDither(true);
         gradientFill.setAntiAlias(true);
         gradientFill.setStyle(Paint.Style.FILL);
+        rect = null;
     }
 
     public void draw(Canvas canvas) {
         super.draw(canvas);
 
-        if (bmp != null) {
-            canvas.drawBitmap(bmp, 0,0, null);
+        if (rect != null) {
+            canvas.drawRect(rect, gradientFill);
         }
     }
-
-    private Bitmap bmp;
 
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
 
-        if (bmp != null) {
-            bmp.recycle();
-            bmp = null;
-            Runtime.getRuntime().gc();
-        }
-
-        RectF rect = new RectF(0.0F, 0.0F, w, h);
+        // Refresh the gradient to match the view size and then repaint
+        rect = new RectF(0.0F, 0.0F, w, h);
         gradientFill.setShader(new RadialGradient(
                 rect.width() / 2.0F,
                 rect.height() / 2.0f,
                 rect.width() * 0.75f,
-                gradientStart,
-                gradientEnd,
+                gradientStartColor,
+                gradientEndColor,
                 Shader.TileMode.CLAMP));
-        gradientFill.setDither(true);
-        bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bmp);
-        canvas.drawRect(rect, gradientFill);
-
 
         postInvalidate();
     }
